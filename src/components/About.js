7f1696a9e514f0e22cf17f6de5dc15fc853192bc
Shajira Guzman <shajirachan@my.unt.edu>
2024-09-30 23:27:07 -0500

    removed exclamation

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,7 @@ export default function About() {
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="lg:text-5xl title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             I'm Shajira.
-            <br className="lg:text-5xl hidden lg:inline-block" />I love to build and create!
+            <br className="lg:text-5xl hidden lg:inline-block" />I love to build and create.
           </h1>
           <p className="mb-8 leading-relaxed">
 
